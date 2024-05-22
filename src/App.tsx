8ea32dd7327b4b9628d2a565bb007c0663cd4d87
Norbaev <timnrbv@gmail.com>
2024-05-22 19:47:05 +0500

    Создал компонент <SearchInfo />

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Card } from './components/Card';
 import { Options } from './components/Options';
+import { SearchInfo } from './components/SearchInfo';
 
 function App() {
   return (
     <main className="min-h-screen	px-[60px] pt-[38px]">
       <Options />
+      <SearchInfo quantity={228} />
       <hr className="my-12 bg-label" />
       <div>
         <Card />
